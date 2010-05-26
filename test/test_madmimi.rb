e7fba7a66f418a868523d911645ab8d513672ed3
Nicholas Young <nicholas@nicholaswyoung.com>
2010-05-26 08:20:19 -0500

    Add a quick test.

--- test/test_madmimi.rb
+++ test/test_madmimi.rb
@@ -1,7 +1,12 @@
 require 'helper'
 
 class TestMadmimi < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "Coming soon!"
+  context "A API call" do
+    setup do
+      @mimi = MadMimi.new('test-user-email', 'test-api-key')
+    end
+    should "return nothing when creating a new list" do
+      assert_equal "", @mimi.new_list('New Test List')
+    end
   end
 end
