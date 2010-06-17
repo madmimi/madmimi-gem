4312968d431241c50b70a17aa8221035caf7f319
Nicholas Young <nicholas@nicholaswyoung.com>
2010-06-17 13:28:09 -0500

    Add new tests

--- test/test_madmimi.rb
+++ test/test_madmimi.rb
@@ -9,7 +9,7 @@ class TestMadmimi < Test::Unit::TestCase
     should "retrieve a hash of promotions" do
       stub_get('/promotions.xml', 'promotions.xml')
       response = @mimi.promotions
-      flunk unless response.kind_of?(Hash) || !response.empty?
+      flunk "I couldn't find any promotions." unless response.kind_of?(Hash) || !response.empty?
     end
     
     should "retrieve a hash of lists" do
@@ -21,7 +21,7 @@ class TestMadmimi < Test::Unit::TestCase
     should "retrieve a hash of users found with the search term nicholas" do
       stub_get('/audience_members/search.xml?query=nicholas', 'search.xml')
       response = @mimi.audience_search('nicholas')
-      flunk "No users found" unless response.kind_of?(Hash) || !response.empty?
+      flunk "No users found." unless response.kind_of?(Hash) || !response.empty?
     end
   end
 end
