340ec58b851ec0ad415bec217ae8a63a6b09e445
Jeff Browning <jeff@jkbrowning.com>
2013-01-15 10:15:48 -0600

    Potential fix for openssl handshake issues between 0.9.8 and 1.x

--- lib/madmimi.rb
+++ lib/madmimi.rb
@@ -216,6 +216,7 @@ class MadMimi
     if transactional == true
       http = Net::HTTP.new(BASE_URL, 443)
       http.use_ssl = true
+      http.ssl_version = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     else
       http = Net::HTTP.new(BASE_URL, 80)
